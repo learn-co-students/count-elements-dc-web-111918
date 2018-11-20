3e5a4fc45a294d313e9a60e907322a45fc165396
github username <github email address>
2018-11-19 21:02:25 -0500

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,13 @@
+require "pry"
 def count_elements(array)
   # code goes here
+  foo = {}
+  array.each {|animal|
+    if foo[animal] == nil
+      foo.store(animal, 1)
+    else
+      foo[animal] += 1
+    end
+  }
+  foo
 end
- 
