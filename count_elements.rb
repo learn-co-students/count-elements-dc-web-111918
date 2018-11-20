1e11572fb5dbe99f36987d7e0c8a704988f2f34e
vovka26 <317110@gmail.com>
2018-11-19 21:34:53 -0500

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,12 @@
+require 'pry'
+
 def count_elements(array)
-  # code goes here
+  new_hash = {}
+  array.each do |element|
+    new_hash[element] ||= 0
+    if new_hash[element] != nil
+      new_hash[element] += 1
+    end
+  end
+  new_hash
 end
- 
